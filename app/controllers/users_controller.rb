11faffe228ae41db2d9489adb7922330f7522021
simp338 <45528081+simp338@users.noreply.github.com>
2019-01-30 12:36:26 +0000

    want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @user_want = @user.want_items.count
   end
 
   def new
